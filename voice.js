fc40a9b4302c3c51c37ae22b7c3a01013f33d671
Lakshay Chawla <lakshaychawla6677@gmail.com>
2022-08-26 15:47:13 +0530

    final release

--- voice.js
+++ voice.js
@@ -1,5 +1,12 @@
-const voice_speech = (text_data)=>{
-    const x = text_data.innerText
-    let utterance = new SpeechSynthesisUtterance(x)
-    speechSynthesis.speak(utterance)
-}
+var voiceSwitch = document.getElementById("soundOn");
+
+const voice_speech = (text_data) => {
+  if (voiceSwitch.checked === true) {
+    const x = text_data.innerText;
+    let utterance = new SpeechSynthesisUtterance(x);
+    speechSynthesis.speak(utterance);
+  }
+};
+
+
+
